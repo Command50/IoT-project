c43a51585dd3910f6b9c31ee7a9867a9aad24757
maksmatviyiv <maksym.matviyiv1998@gmail.com>
2018-09-27 19:58:10 +0300

    Fix XML, add two activivty for sensor

--- app/src/main/java/com/example/maksy/testproject/ListSensorsActivity.java
+++ app/src/main/java/com/example/maksy/testproject/ListSensorsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ListSensorsActivity extends AppCompatActivity {
 
@@ -11,9 +12,15 @@ public class ListSensorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_sensors);
+
+
     }
     public void onDetailPage(View view) {
         Intent intent = new Intent(ListSensorsActivity.this, DetailPageActivivty.class);
+        startActivity(intent);
+    }
+    public void onDetailPageTwo (View view) {
+        Intent intent = new Intent(ListSensorsActivity.this, DetailPageActivityTwo.class);
         startActivity(intent);
     }
 }
