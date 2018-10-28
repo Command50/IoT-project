d0bbfb3529a70262529d06b087b49cf5c17458ff
vladglushko1006 <vladglushko1006@gmail.com>
2018-10-28 17:04:17 +0200

    Add condition about temperature

--- sketch_oct02a.ino
+++ sketch_oct02a.ino
@@ -2,8 +2,11 @@
 
 //OneWire ds(2);
 
+int Relay = 2;
+
 void setup() {
     Serial.begin(115200);
+    pinMode(Relay, OUTPUT);
 }
 
 void loop() {
@@ -15,5 +18,13 @@ int reading = analogRead(sensorPin);
   float temperatureC = voltage * 25;
    Serial.print(temperatureC); Serial.println("degress C");
    delay(1000);
+  if (temperatureC > 50) {
+    digitalWrite(Relay, LOW);  
+    }
+    else {
+      digitalWrite(Relay, LOW); // реле увімкнено
+      delay(2000);
+      digitalWrite(Relay, HIGH); // реле вимкнено
+      delay(2000);  
+    }
   }
-    
